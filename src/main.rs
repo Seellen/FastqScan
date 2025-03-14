232814f768fa2cd799634d93d802023ede8cd6cd
Alen Özcan <aleo01@zedat-fu.de>
2025-03-14 12:28:05 +0100

    Added tests and first calc_phred func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#[cfg(test)]
+mod test {
+
+    use super::calculate_phred;
+    
+    #[test]
+    fn test_calculate_phred() {
+        let qual: char = '&';
+        let expected:usize =	5; // Phred-Score für das Zeichen '&'
+        let res = calculate_phred(qual);
+        assert_eq!(expected, res);
+    }
+
+    // Alternativ können viele Ergebnisse auf einmal getestet werden:
+    #[test]
+    fn test_calculate_phred_other() {
+        let tests: Vec<(char, usize)> = vec![
+            ('&', 5),
+            ('+', 10)
+        ];
+        for test in tests {
+            let res = calculate_phred(test.0);
+            assert_eq!(test.1, res);
+        }
+    }
+    // ggf. andere Testfunktionen
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+pub fn calculate_phred(qual: char) -> u8 {
+    let phsc = qual as u8;
+    phsc - 33
 }
