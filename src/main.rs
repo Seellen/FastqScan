ae256ac2a44d41179e7cc9b050bfb6fc62ef8c6c
Alen Özcan <aleo01@zedat-fu.de>
2025-03-14 15:36:36 +0100

    Created structs not workint 100% yet

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::io;
 mod phred; 
+mod getinfo;
 
 
 
 fn main() {
-    println!("Please input ur Phred seq:");
+    println!("\nPlease input ur Phred seq:");
 
     let mut phred_seq = String::new();
 
@@ -18,5 +19,10 @@ fn main() {
         Some(avg) => println!("The average Phred Score of your sequence is: {avg}"),
         None => println!("Invalid Char found in String"),
     }
+
+    let mut x = "NIST7035_TAAGGCGA_L001_R1_001.fastq.gz".to_string();
+    let mut y = "@HWI-D00119:50:H7AP8ADXX:1:1101:2100:2202 1:N:0:TAAGGCGA".to_string();
+    getinfo::info_data(&mut x);
+    getinfo::info_read(&mut y);
 
 }
