c3180cc858f0c16d68dbdf477822eb2caf8dd6b1
Alen Özcan <aleo01@zedat-fu.de>
2025-03-20 10:44:24 +0100

    We have tables now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use clap::{ArgGroup, Parser};
+use clap::Parser;
 use std::path::{Path, PathBuf};
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+use crate::phred::avg_qual;
+use flate2::read::GzDecoder;
+use prettytable::{Table, row, cell};
 mod phred; 
 mod getinfo;    
 mod calls;
@@ -8,11 +13,7 @@ mod tests;
 
 #[derive(Parser)]
 #[command(name = "FASTQ Parser", version = "1.0", about = "Liest FASTQ-Dateien ein")]
-#[command(group(
-    ArgGroup::new("input")
-        .required(true)
-        .args(["r1", "r2"]),
-))]
+
 struct Args {
     /// Pfad zur R1 FASTQ-Datei
     #[arg(short = '1', long)]
@@ -23,7 +24,26 @@ struct Args {
     r2: Option<PathBuf>,
 }
 
+fn process_fastq(file_path: PathBuf) {
+    // open and use file
+    let file = File::open(file_path).expect("Konnte die Datei nicht öffnen");
+    let decoder = GzDecoder::new(file); // Entpacke Gzip
+    let reader = BufReader::new(decoder);
 
+    let mut table = Table::new();
+    table.add_row(row!["Position", "Avg Phred score"]);
+
+    let mut line_count = 0;
+    for line in reader.lines().filter_map(Result::ok) {
+        line_count += 1;
+        if line_count % 4 == 0 {  // Qualitätsscore-Zeilen in FASTQ-Dateien sind jede 4. Zeile
+            let avg_score = avg_qual(line.as_bytes());
+            table.add_row(row![line_count/4,format!("{:?}", avg_score)]);
+        }
+    }
+
+    table.printstd();
+}
 
 
 fn main() {
@@ -38,7 +58,8 @@ fn main() {
         std::process::exit(1);
     }
     
-    println!("R1-Datei: {:?}", args.r1);
+    println!("\nR1-Datei: {:?}", args.r1);
+    process_fastq(args.r1);
     
     // ---- READ 2 -----
     if let Some(r2) = args.r2 {
@@ -47,7 +68,9 @@ fn main() {
             eprintln!("Fehler: Die R2-Datei '{:?}' existiert nicht!", r2);
             std::process::exit(1);
         }
-        println!("R2-Datei: {:?}", r2);
+        _read2 = r2.clone();
+        println!("\nR2-Datei: {:?}", r2);
+        process_fastq(_read2);
     } else {
         println!("Nur Single-End Datei angegeben.");
     }
