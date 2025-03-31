06098efe07b57394821681d853f0beb59ffd3f9e
Alen Özcan <aleo01@zedat-fu.de>
2025-03-31 12:02:24 +0200

    Lengths is a distribution

--- src/statistics/read_data.rs
+++ src/statistics/read_data.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Default, Serialize, Deserialize)]
 pub struct ReadData {
-    read_lengths: Vec<usize>,
+    read_lengths: Vec<(usize, usize)>,
 }
 
 impl ReadData {
@@ -17,6 +17,12 @@ impl ReadData {
 #[typetag::serde]
 impl Statistic for ReadData {
     fn process(&mut self, record: &FastqRecord) {
-        self.read_lengths.push(record.seq.len());
+
+        let len = record.seq.len();
+        if let Some(pos) = self.read_lengths.iter().position(|(l, _)| *l == len) {
+            self.read_lengths[pos].1 += 1;
+        } else {
+            self.read_lengths.push((len, 1));
+        }
     }
 }
